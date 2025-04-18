aa5f10cbd2c37a6a49eedc399c6685ac80070d38
Akhmedovdeveloper01 <developerakhmedov@gmail.com>
2025-04-18 11:04:32 +0500

    ...

---
+++ src/components/component/Calculator.jsx
@@ -0,0 +1,95 @@
+import { useState, useEffect, useRef } from "react";
+import { evaluate } from "mathjs";
+
+const Calculator = () => {
+    const [display, setDisplay] = useState("0");
+    const btns = [
+        "7",
+        "8",
+        "9",
+        "/",
+        "4",
+        "5",
+        "6",
+        "*",
+        "1",
+        "2",
+        "3",
+        "-",
+        "0",
+        ".",
+        "=",
+        "+",
+        "C",
+        "X",
+    ];
+    const calculatorRef = useRef(null);
+
+    const handleButtonClick = (value) => {
+        if (value === "=") {
+            try {
+                setDisplay(evaluate(display).toString());
+            } catch {
+                setDisplay("Xato");
+            }
+        } else if (value === "C") {
+            setDisplay("0");
+        } else if (value === "X") {
+            setDisplay((prev) => {
+                if (prev === "0" || prev === "Xato") return "0";
+                if (prev.length === 1) return "0";
+                return prev.slice(0, -1);
+            });
+        } else {
+            setDisplay((prev) =>
+                prev === "0" || prev === "Xato" ? value : prev + value
+            );
+        }
+    };
+
+    const handleKeyDown = (event) => {
+        const { key } = event;
+
+        if (/[0-9+\-*/.]/.test(key)) {
+            handleButtonClick(key);
+        } else if (key === "Enter") {
+            handleButtonClick("=");
+        } else if (key === "Backspace") {
+            handleButtonClick("X");
+        } else if (key === "Escape") {
+            handleButtonClick("C");
+        }
+
+        if (key === "Enter" || key === "/") {
+            event.preventDefault();
+        }
+    };
+
+    useEffect(() => {
+        calculatorRef.current.focus();
+    }, []);
+
+    return (
+        <div
+            className="w-[350px] border border-[#ccc]  mt-10"
+            tabIndex={0}
+            onKeyDown={handleKeyDown}
+            ref={calculatorRef}
+        >
+            <div className=" p-4 text-2xl text-right">{display}</div>
+            <div className="grid grid-cols-4 gap-px border-t border-[#ccc] bg-[#ccc]">
+                {btns.map((btn) => (
+                    <button
+                        className="p-5 text-[18px] cursor-pointer hover:bg-[#e0e0e0] border border-[#ccc]"
+                        key={btn}
+                        onClick={() => handleButtonClick(btn)}
+                    >
+                        {btn}
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Calculator;
--- src/page/Setting/Setting.jsx
+++ src/page/Setting/Setting.jsx
@@ -1,86 +1,96 @@
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useTheme } from "@/providers/ThemeProvider";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { Sun, Moon, Monitor } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import Calculator from "@/components/component/Calculator";
 
 export default function Setting() {
-  const { i18n } = useTranslation();
-  const { theme, setTheme } = useTheme();
-  const { t } = useTranslation();
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
-    localStorage.setItem("language", lng);
-  };
-  return (
-    <div>
-      <div>
-        <h2 className="text-textPrimaryColor dark:text-white font-semibold text-[16px] mb-1 mt-4">
-          {t("languageChange")}:
-        </h2>
-        <DropdownMenu>
-          <DropdownMenuTrigger className="px-4 py-1.5 rounded-lg bg-gray-700 text-white hover:bg-gray-600">
-            {i18n.language.toUpperCase()}
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem
-              onClick={() => changeLanguage("en")}
-              className={i18n.language === "en" ? "bg-blue-500/10" : ""}
-            >
-              English
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              onClick={() => changeLanguage("uz")}
-              className={i18n.language === "uz" ? "bg-blue-500/10" : ""}
-            >
-              O'zbekcha
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              onClick={() => changeLanguage("ru")}
-              className={i18n.language === "ru" ? "bg-blue-500/10" : ""}
-            >
-              Русский
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-      <div>
-        <h2 className="text-textPrimaryColor dark:text-white font-semibold text-[16px] mb-1 mt-4">
-          {t("themeChange")}:
-        </h2>
-        <div className="flex items-center tablet:gap-4 gap-3">
-          <Button
-            variant={theme === "light" ? "default" : "outline"}
-            className="justify-start tablet:max-w-[140px] max-w-[100px] text-[13px] tablet:text-[14px] tablet:py-5 py-4"
-            onClick={() => setTheme("light")}
-          >
-            <Sun className="h-4 w-4" />
-            {t("light")}
-          </Button>
-          <Button
-            variant={theme === "dark" ? "default" : "outline"}
-            className="justify-start tablet:max-w-[140px] text-[13px] max-w-[100px] tablet:text-[14px] tablet:py-5 py-4"
-            onClick={() => setTheme("dark")}
-          >
-            <Moon className="h-3 w-3 tablet:h-4 tablet:w-4" />
-            {t("dark")}
-          </Button>
-          <Button
-            variant={theme === "system" ? "default" : "outline"}
-            className="justify-start tablet:max-w-[140px] max-w-[100px] text-[13px] tablet:text-[14px] tablet:py-5 py-4"
-            onClick={() => setTheme("system")}
-          >
-            <Monitor className="h-4 w-4" />
-            {t("system")}
-          </Button>
+    const { i18n } = useTranslation();
+    const { theme, setTheme } = useTheme();
+    const { t } = useTranslation();
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+        localStorage.setItem("language", lng);
+    };
+    return (
+        <div>
+            <div>
+                <h2 className="text-textPrimaryColor dark:text-white font-semibold text-[16px] mb-1 mt-4">
+                    {t("languageChange")}:
+                </h2>
+                <DropdownMenu>
+                    <DropdownMenuTrigger className="px-4 py-1.5 rounded-lg bg-gray-700 text-white hover:bg-gray-600">
+                        {i18n.language.toUpperCase()}
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuItem
+                            onClick={() => changeLanguage("en")}
+                            className={
+                                i18n.language === "en" ? "bg-blue-500/10" : ""
+                            }
+                        >
+                            English
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                            onClick={() => changeLanguage("uz")}
+                            className={
+                                i18n.language === "uz" ? "bg-blue-500/10" : ""
+                            }
+                        >
+                            O'zbekcha
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                            onClick={() => changeLanguage("ru")}
+                            className={
+                                i18n.language === "ru" ? "bg-blue-500/10" : ""
+                            }
+                        >
+                            Русский
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+            <div>
+                <h2 className="text-textPrimaryColor dark:text-white font-semibold text-[16px] mb-1 mt-4">
+                    {t("themeChange")}:
+                </h2>
+                <div className="flex items-center tablet:gap-4 gap-3">
+                    <Button
+                        variant={theme === "light" ? "default" : "outline"}
+                        className="justify-start tablet:max-w-[140px] max-w-[100px] text-[13px] tablet:text-[14px] tablet:py-5 py-4"
+                        onClick={() => setTheme("light")}
+                    >
+                        <Sun className="h-4 w-4" />
+                        {t("light")}
+                    </Button>
+                    <Button
+                        variant={theme === "dark" ? "default" : "outline"}
+                        className="justify-start tablet:max-w-[140px] text-[13px] max-w-[100px] tablet:text-[14px] tablet:py-5 py-4"
+                        onClick={() => setTheme("dark")}
+                    >
+                        <Moon className="h-3 w-3 tablet:h-4 tablet:w-4" />
+                        {t("dark")}
+                    </Button>
+                    <Button
+                        variant={theme === "system" ? "default" : "outline"}
+                        className="justify-start tablet:max-w-[140px] max-w-[100px] text-[13px] tablet:text-[14px] tablet:py-5 py-4"
+                        onClick={() => setTheme("system")}
+                    >
+                        <Monitor className="h-4 w-4" />
+                        {t("system")}
+                    </Button>
+                </div>
+            </div>
+            <div>
+                <Calculator />
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
