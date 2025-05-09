7cd5d843bc762026b5dd6ca813943c75f418a4d8
Akhmedovdeveloper01 <developerakhmedov@gmail.com>
2025-05-09 15:22:46 +0500

    ...

--- src/page/Clients/Clients.jsx
+++ src/page/Clients/Clients.jsx
@@ -8,135 +8,165 @@ import OptionImg from "@/assets/imgs/optional-img.jpg";
 
 import ClientDrawer from "./ClientDrawer";
 import { NumberFormatter } from "@/components/component/Number-Formatter";
+import { useQuery } from "@tanstack/react-query";
+import {
+    getClientsBusinessQuery,
+    getClientsCustomersQuery,
+} from "@/quires/quires";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Clients() {
-  // const [page, setPage] = useState(1);
-  const [selectedRowData, setSelectedRowData] = useState(null);
-  const [isSheetOpen, setIsSheetOpen] = useState(false);
-  const limit = 1000;
-  const [searchTerm, setSearchTerm] = useState("");
+    // const [page, setPage] = useState(1);
+    const [selectedRowData, setSelectedRowData] = useState(null);
+    const [isSheetOpen, setIsSheetOpen] = useState(false);
+    const limit = 1000;
+    const [searchTerm, setSearchTerm] = useState("");
 
-  const { data: clientCustomer } = useGetData({
-    endpoint: "/client-customers",
-    enabled: true,
-    params: { limit, search: searchTerm },
-    getQueryKey: "/clients",
-  });
+    // const { data: clientCustomer } = useGetData({
+    //     endpoint: "/client-customers",
+    //     enabled: true,
+    //     params: { limit, search: searchTerm },
+    //     getQueryKey: "/clients",
+    // });
 
-  // const { data: clientBusiness } = useGetData({
-  //   endpoint: "/client-businesses",
-  //   enabled: true,
-  //   params: { limit, search: searchTerm },
-  //   getQueryKey: "/clients",
-  // });
+    const { data: clientCustomer } = useQuery({
+        ...getClientsCustomersQuery({ limit: "10", page: "1", search: "" }),
+    });
+    const { data: clientBusiness } = useQuery({
+        ...getClientsBusinessQuery({ limit: "10", page: "1", search: "" }),
+    });
 
-  // const clientCheck = clientCustomer?.Data?.customers?.map((item) => {
-  //   return {
-  //     ...item,
-  //     type: "customer",
-  //   };
-  // });
+    // const { data: clientBusiness } = useGetData({
+    //   endpoint: "/client-businesses",
+    //   enabled: true,
+    //   params: { limit, search: searchTerm },
+    //   getQueryKey: "/clients",
+    // });
 
-  // console.log(clientCheck);
-  // const clientBusinessCheck = clientBusiness?.Data?.businesses?.map((item) => {
-  //   return {
-  //     ...item,
-  //     type: "business",
-  //   };
-  // });
-  // console.log(clientBusinessCheck);
+    // const clientCheck = clientCustomer?.Data?.customers?.map((item) => {
+    //   return {
+    //     ...item,
+    //     type: "customer",
+    //   };
+    // });
 
-  const infoClick = (row) => () => {
-    setSelectedRowData(row);
-    setIsSheetOpen(true);
-  };
+    // console.log(clientCheck);
+    // const clientBusinessCheck = clientBusiness?.Data?.businesses?.map((item) => {
+    //   return {
+    //     ...item,
+    //     type: "business",
+    //   };
+    // });
+    // console.log(clientBusinessCheck);
 
-  const column = [
-    {
-      header: "No",
-      cell: ({ row }) => {
-        return <div>{row.index + 1}</div>;
-      },
-    },
-    {
-      header: "image",
-      cell: ({ row }) => {
-        return (
-          <div>
-            <img
-              src={row?.original?.file || OptionImg}
-              alt=""
-              className="w-[80px] h-[55px] rounded-md object-cover"
+    const infoClick = (row) => () => {
+        setSelectedRowData(row);
+        setIsSheetOpen(true);
+    };
+
+    const column = [
+        {
+            header: "No",
+            cell: ({ row }) => {
+                return <div>{row.index + 1}</div>;
+            },
+        },
+        {
+            header: "image",
+            cell: ({ row }) => {
+                return (
+                    <div>
+                        <img
+                            src={row?.original?.file || OptionImg}
+                            alt=""
+                            className="w-[80px] h-[55px] rounded-md object-cover"
+                        />
+                    </div>
+                );
+            },
+        },
+        {
+            accessorKey: "first_name",
+            header: "name",
+        },
+        {
+            accessorKey: "last_name",
+            header: "lastName",
+        },
+        {
+            accessorKey: "region",
+            header: "region",
+        },
+        {
+            accessorKey: "district",
+            header: "district",
+        },
+        {
+            header: "phoneNumber",
+            cell: ({ row }) => {
+                return <NumberFormatter phone={row.original.phone} />;
+            },
+        },
+        {
+            header: "createdAt",
+            cell: ({ row }) => {
+                return (
+                    <div>
+                        {dayjs(row.original.created_at).format("DD/MM/YYYY")}
+                    </div>
+                );
+            },
+        },
+
+        {
+            header: "actions",
+            cell: ({ row }) => {
+                // console.log(row.original.id);
+                return (
+                    <div className="flex gap-3">
+                        <ClientDrawer
+                            isSheetOpen={isSheetOpen}
+                            setIsSheetOpen={setIsSheetOpen}
+                            row={row}
+                            selectedRowData={selectedRowData}
+                            infoClick={infoClick}
+                        />
+                    </div>
+                );
+            },
+        },
+    ];
+
+    return (
+        <div>
+            <DynamicHeader
+                title="clients"
+                btnName="create"
+                inputPlacholder="searchClient"
+                btnNavigate="/createClient"
+                onSearch={(value) => setSearchTerm(value)}
+                isInput={true}
             />
-          </div>
-        );
-      },
-    },
-    {
-      accessorKey: "first_name",
-      header: "name",
-    },
-    {
-      accessorKey: "last_name",
-      header: "lastName",
-    },
-    {
-      accessorKey: "region",
-      header: "region",
-    },
-    {
-      accessorKey: "district",
-      header: "district",
-    },
-    {
-      header: "phoneNumber",
-      cell: ({ row }) => {
-        return <NumberFormatter phone={row.original.phone} />;
-      },
-    },
-    {
-      header: "createdAt",
-      cell: ({ row }) => {
-        return <div>{dayjs(row.original.created_at).format("DD/MM/YYYY")}</div>;
-      },
-    },
+            <Tabs defaultValue="customers" className="w-full mt-6">
+                <TabsList>
+                    <TabsTrigger value="customers">Jismoniy shaxs</TabsTrigger>
+                    <TabsTrigger value="businesses">Yuridik shaxs</TabsTrigger>
+                </TabsList>
+                {/* shu table larni ikki xil dataga moslashtirish kerak, keyin drawerni ham tekshirish kerak */}
+                <TabsContent value="customers">
+                    <DataTable
+                        data={clientCustomer?.data?.Data?.customers || []}
+                        columns={column}
+                    />
+                </TabsContent>
 
-    {
-      header: "actions",
-      cell: ({ row }) => {
-        // console.log(row.original.id);
-        return (
-          <div className="flex gap-3">
-            <ClientDrawer
-              isSheetOpen={isSheetOpen}
-              setIsSheetOpen={setIsSheetOpen}
-              row={row}
-              selectedRowData={selectedRowData}
-              infoClick={infoClick}
-            />
-          </div>
-        );
-      },
-    },
-  ];
-
-  return (
-    <div>
-      <DynamicHeader
-        title="clients"
-        btnName="create"
-        inputPlacholder="searchClient"
-        btnNavigate="/createClient"
-        onSearch={(value) => setSearchTerm(value)}
-        isInput={true}
-      />
-
-      <div className="mt-6">
-        <DataTable
-          data={clientCustomer?.Data?.customers || []}
-          columns={column}
-        />
-      </div>
-    </div>
-  );
+                <TabsContent value="businesses">
+                    <DataTable
+                        data={clientBusiness?.data?.Data?.businesses || []}
+                        columns={column}
+                    />
+                </TabsContent>
+            </Tabs>
+        </div>
+    );
 }
--- src/service/client.js
+++ src/service/client.js
@@ -3,3 +3,11 @@ import apiClient from "@/config/apiClient";
 export const getClientById = (id) => {
     return apiClient({ method: "get", url: `/client-customer/${id}` });
 };
+
+export const getClientsBusiness = (params) => {
+    return apiClient({ method: "get", url: `/client-businesses`, params });
+};
+
+export const getClientsCustomers = (params) => {
+    return apiClient({ method: "get", url: `/client-customers`, params });
+};
