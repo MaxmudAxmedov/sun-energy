c28123396ce77eaddb3338c046300d2bc1c368a0
Akhmedovdeveloper01 <developerakhmedov@gmail.com>
2025-05-24 21:28:27 +0500

    ....

--- src/page/Cost/Cost.jsx
+++ src/page/Cost/Cost.jsx
@@ -36,7 +36,7 @@ export default function Cost() {
         },
         {
             accessorKey: "description",
-            header: "description",
+            header: "info",
         },
 
         {
